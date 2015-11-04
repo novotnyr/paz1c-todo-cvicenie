9e079ad6145ba6f80c02bcac2a13f135c7d63080
student <student@158.197.35.78>
2015-11-04 12:02:41 +0100

    Pouzitie DefaultListCellRenderera pre polozky

--- src/main/java/sk/upjs/ics/todo/UlohaListCellRenderer.java
+++ src/main/java/sk/upjs/ics/todo/UlohaListCellRenderer.java
@@ -2,20 +2,21 @@ package sk.upjs.ics.todo;
 
 import java.awt.Color;
 import java.awt.Component;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
-public class UlohaListCellRenderer implements ListCellRenderer<Uloha>{
+public class UlohaListCellRenderer extends DefaultListCellRenderer {
 
     @Override
-    public Component getListCellRendererComponent(JList<? extends Uloha> list, Uloha uloha, int index, boolean isSelected, boolean cellHasFocus) {
-        JLabel ulohaLabel = new JLabel(uloha.getNazov());        
-        if(jeSplnena(uloha)) {
-            ulohaLabel.setOpaque(true);
-            ulohaLabel.setBackground(Color.GREEN);            
+    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus); //To change body of generated methods, choose Tools | Templates.
+        Uloha uloha = (Uloha) value;
+        if(jeSplnena(uloha) && !isSelected) {
+            component.setBackground(Color.GREEN);
         }
-        return ulohaLabel;
+        return component;
     }
 
     private boolean jeSplnena(Uloha uloha) {
